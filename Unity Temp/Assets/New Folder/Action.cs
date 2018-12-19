fd3b89ec7b7082926e77b5093c8110df4db8e461
MidScout <45867846+MidScout@users.noreply.github.com>
2018-12-19 08:56:16 +0800

    Unity场景

---
+++ Unity Temp/Assets/New Folder/Action.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action : MonoBehaviour {
+
+    //public float movespeed;
+    // Use this for initialization
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+
+    private Vector3 moveDirection = Vector3.zero;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+        if (controller.isGrounded)
+        {
+            moveDirection = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+        //float MoveH = Input.GetAxis("Horizontal");
+        //float MoveV = Input.GetAxis("Vertical");
+        //transform.Translate(new Vector3(MoveH, 0, MoveV) * Time.deltaTime * movespeed);    //按水平或者垂直的方向键，                                                                                                                                              就怎么移动
+        //if (Input.GetButton("Jump"))
+        //{
+        //    Rigidbody r = GetComponent<Rigidbody>();
+        //    r.AddForce(new Vector3(0, 0.5f, 0), ForceMode.Impulse);
+        //
+}
---
+++ Unity Temp/Assets/New Folder/Carmine.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carmine : MonoBehaviour
+{
+
+
+    //public float sensitivityX = 10f;
+    //public float sensitivityY = 10f;
+
+    public float sensitivityMouse = 2f;
+
+    void Update()
+   {
+    //    //鼠标右键实现视角转动，类似第一人称视角
+    //    if (Input.GetMouseButton(1))
+    //    {
+    //        float rotationX = Input.GetAxis("Mouse X") * sensitivityX;
+    //        float rotationY = Input.GetAxis("Mouse Y") * sensitivityY;
+    //        transform.Rotate(-rotationY, rotationX, 0);
+    //    }
+        if (Input.GetMouseButton(1))
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X"),0);
+        }
+    }
+
+
+}
